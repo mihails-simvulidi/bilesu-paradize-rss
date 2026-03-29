5b1d79c26db8c61e72e75f8848e2bce2077dddc4
Mihails Simvulidi <mihails@simvulidi.lv>
2026-03-29 22:11:06 +0000

    Remove fake-useragent dependency and update user agent handling in rss_proxy function

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from bs4 import BeautifulSoup, Tag
-from fake_useragent import UserAgent
 from flask import Flask, Response
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 from xml.etree import ElementTree as ET
 import requests
 
 BASE_URL = "https://www.bilesuparadize.lv"
+BASE_HOST = urlparse(BASE_URL).netloc
+REQUEST_TIMEOUT = (3.05, 10)
+USER_AGENT = "bilesu-paradize-rss/1.0 (+https://github.com/mihails-simvulidi/bilesu-paradize-rss)"
 
 app = Flask(__name__)
 
@@ -20,10 +22,16 @@ def home():
 
 @app.route("/<path:path>")
 def rss_proxy(path: str):
-    user_agent = UserAgent()
-    source_url = urljoin(BASE_URL, path)
-    response = requests.get(source_url, headers={"User-Agent": user_agent.random})
+
+    # Fetch the source page
+    source_url = f"{BASE_URL}/{path}"
+    response = requests.get(
+        source_url,
+        headers={"User-Agent": USER_AGENT},
+    )
     response.raise_for_status()
+
+    # Set channel attributes
     soup = BeautifulSoup(response.text, "html.parser")
     rss = ET.Element("rss", attrib={"version": "2.0"})
     channel = ET.SubElement(rss, "channel")
@@ -34,6 +42,8 @@ def rss_proxy(path: str):
     title_element = soup.find("title")
     if isinstance(title_element, Tag):
         ET.SubElement(channel, "title").text = title_element.text
+
+    # Extract event items
     event_elements = soup.find_all("div", attrs={"role": "listitem"})
     for event_element in event_elements:
         link_element = event_element.find("a", class_="scoped-date")
@@ -42,6 +52,8 @@ def rss_proxy(path: str):
         ET.SubElement(item, "guid").text = event_url
         ET.SubElement(item, "link").text = event_url
         ET.SubElement(item, "title").text = link_element.text
+
+    # Return the RSS feed as XML
     ET.indent(rss, space="  ")
     xml_bytes = ET.tostring(rss, encoding="utf-8", xml_declaration=True)
     rss_string = f"{xml_bytes.decode('utf-8')}\n"
